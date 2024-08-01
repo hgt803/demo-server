479a9752828ba74b86d6e7ee375446acf886511c
cake <hgt803@gmail.com>
2024-08-01 13:04:31 +0800

    fix: 验证手机号码

--- app/controller/api/verificationCode.js
+++ app/controller/api/verificationCode.js
@@ -10,6 +10,19 @@ class GetVerificationCodeController extends Controller {
     const { ctx } = this;
     const { phoneNumber } = ctx.request.body;
 
+    var reg = /^1[3456789]\d{9}$/;
+    // ^1  以1开头
+    // [3456789] 第2位，使用原子表里的任意一个原子都可以
+    // \d{9}$  第三位  朝后可以是任意数字  并且最后结尾必须是数字
+
+    if(!reg.test(phoneNumber)){
+      ctx.body = {
+        errCode: 1010,
+        errMsg: '请输入正确的手机号码',
+        verificationCode: '',
+      };
+      return;
+    }
 
     const verificationCode = getRandomIntInRange(1000, 9999);
 
